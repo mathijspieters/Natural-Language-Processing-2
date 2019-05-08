b63c8f085d3dcc8d9ad7179f02ee614ec610b71e
Rob <larry@wcw-stud-145-109-58-207.wireless.uva.nl>
2019-05-08 11:21:43 +0200

    alsjeblieft janne

--- Assignment2/decoder.py
+++ Assignment2/decoder.py
@@ -1,9 +1,12 @@
 import torch
 import torch.nn as nn
 
-class RNNLM(nn.Module):
+class Decoder(nn.Module):
     def __init__(self, vocab_size, emb_size, hidden_size=256, latent_size=20, num_layers=1):
         super().__init__()
+
+        self.z2hidden = nn.Linear(latent_size, 2*hidden_size)
+        self.z_act = nn.Tanh()
 
         self.embedding = nn.Embedding(vocab_size, emb_size)
         self.rnn = nn.GRU(input_size=emb_size, hidden_size=hidden_size,
@@ -11,9 +14,12 @@ class RNNLM(nn.Module):
         self.hidden2out = nn.Linear(hidden_size, latent_size)
         self.act = nn.Softmax(dim=-1)
 
-    def forward(self, input):
+    def forward(self, input, z):
+        hidden = self.z_act(self.z2hidden(z))
+        hidden = hidden.chunk(2, dim=-1)
+
         out = self.embedding(input)
-        out, _ = self.rnn(out)
+        out, _ = self.rnn(out, hidden)
         out = self.hidden2out(out)
         out = self.act(out)
         return out
--- Assignment2/test.py
+++ Assignment2/test.py
@@ -1,15 +1,21 @@
 import torch
 from decoder import Decoder
+from RNNLM import RNNLM
+from encoder import Encoder
 
 vocab_size = 100
 emb_size = 50
 hidden_size = 30
 latent_size = 10
 
+seq_len = 3
+batch_size = 2
+
 henk = Decoder(vocab_size, emb_size, hidden_size, latent_size)
 
-a = torch.randint(0, vocab_size, (3, 2)).long()
-print(a)
-b = henk(a)
+input = torch.randint(0, vocab_size, (seq_len, batch_size)).long()
+z = torch.normal(torch.zeros(batch_size, latent_size), 1)
+print(input)
+b = henk(input, z)
 
 print(b.sum(-1))

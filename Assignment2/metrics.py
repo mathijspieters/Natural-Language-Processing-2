1df5a75c0300f169d2c595ee3585025403fe4abd
Rob <larry@wcw-stud-145-109-2-144.wireless.uva.nl>
2019-05-15 12:15:14 +0200

    ppl_added

--- Assignment2/metrics.py
+++ Assignment2/metrics.py
@@ -1,7 +1,9 @@
-def ppl(out, targets, lengths):
+def ppl(out, targets, mask):
     """ Compute perplexity """
-    # Todo how about padding tokens?
+    # Remove the effect of irrelevant tokens.
+    out[mask==0] = 1
+
+    #Collect the probs of the targets.
     likelihood = out.gather(dim=-1, index=targets.unsqueeze(-1)).squeeze()
     likelihood = likelihood.prod(dim=0)
-
-    return (-likelihood.log().sum()/lengths.sum()).exp()
+    return (-likelihood.log().sum()/mask.sum()).exp()

40717ae86020f59069964376a33c41cdb3cdcd8d
Mathijs Pieters <>
2019-05-17 17:03:59 +0200

    Update for numerical stability, still gives NAN?

--- Assignment2/metrics.py
+++ Assignment2/metrics.py
@@ -1,9 +1,12 @@
+import torch
+
 def ppl(out, targets, mask):
     """ Compute perplexity """
     # Remove the effect of irrelevant tokens.
     out[mask==0] = 1
 
     #Collect the probs of the targets.
+    out = torch.nn.functional.softmax(out, dim=-1)
     likelihood = out.gather(dim=-1, index=targets.unsqueeze(-1)).squeeze()
-    likelihood = likelihood.prod(dim=0)
-    return (-likelihood.log().sum()/mask.sum()).exp()
+    likelihood = likelihood.log().sum(dim=0)
+    return (-likelihood.sum()/mask.sum()).exp()

d9ee886d207559e22e4fc56fa518885d691fcdf5
Mathijs Pieters <>
2019-05-24 12:53:36 +0200

    Replaced code to metrics Perplexity calculation

--- Assignment2/metrics.py
+++ Assignment2/metrics.py
@@ -1,5 +1,14 @@
 import torch
 
+def KL(mu, sigma):
+    loss = -0.5 * torch.sum(1 + sigma.log() - mu.pow(2) - sigma)
+    return loss
+
+def ACC(predictions, targets, masks, lengths):
+    predictions[masks == 0] = -1
+    correct = ((predictions == targets).sum(dim=0).float() / lengths.float()).mean()
+    return correct
+
 def ppl(out, targets, mask):
     """ Compute perplexity """
     # Remove the effect of irrelevant tokens.
@@ -8,8 +17,8 @@ def ppl(out, targets, mask):
     #Collect the probs of the targets.
     out = torch.nn.functional.softmax(out, dim=-1)
     likelihood = out.gather(dim=-1, index=targets.unsqueeze(-1)).squeeze()
-    likelihood = likelihood.log().sum(dim=0)
-    return (-likelihood.sum()/mask.sum()).exp()
+    likelihood = likelihood.log().sum()
+    return -likelihood
 
 def approx_likelihood(SentVAE, inputs, targets, S=10):
 

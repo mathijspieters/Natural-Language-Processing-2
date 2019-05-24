243e14d344ce935dc5c7c126a0fae34607b4f6b0
Mathijs Pieters <>
2019-05-24 12:53:05 +0200

    Added latent interpolation

--- Assignment2/sent_vae.py
+++ Assignment2/sent_vae.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from encoder import Encoder
 from decoder import Decoder
+import utils
 
 class SentVAE(nn.Module):
     def __init__(self, vocab_size, emb_size, hidden_size, latent_size, num_layers, pad_idx, sos_idx, device):
@@ -26,5 +27,15 @@ class SentVAE(nn.Module):
             start_input = torch.ones(1, n_samples, dtype=torch.long).fill_(self.SOS).to(self.device)
 
             out = self.decoder.generate(start_input, z, 30)
+
+        return out
+
+    def interpolation(self, n_steps=10):
+        with torch.no_grad():
+            z_interpolation = utils.interpolation(n_steps, self.latent_size)
+            interpolation_z =  torch.from_numpy(z_interpolation).float().to(self.device)
+            start_input = torch.ones(1, n_steps, dtype=torch.long).fill_(self.SOS).to(self.device)
+            
+            out = self.decoder.generate(start_input, interpolation_z, 30)
 
         return out

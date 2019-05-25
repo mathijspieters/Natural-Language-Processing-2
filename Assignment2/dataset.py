- Correct calculation of PPL - Added use of validation set - Added paper

Assignment2/dataset.py
@@ -40,16 +40,14 @@ class Dataset(data.Dataset):
 
     def process(self, data):
         tmp = []
-        punctuation = string.punctuation
+        #punctuation = string.punctuation
         for i, d in enumerate(data.readlines()):
             t = Tree.fromstring(d)
             sentence = t.leaves()
-            sentence = [w.lower() for w in sentence]
-            sentence = [w for w in sentence if w not in punctuation]
+            #sentence = [w.lower() for w in sentence]
+            #sentence = [w for w in sentence if w not in punctuation]
             self.count += Counter(sentence)
             tmp.append(sentence)
-            # if i == 99:
-            #     break
         return tmp
 
     def create_vocabulary(self):
@@ -117,20 +115,34 @@ class DataLoader:
     def permute(self):
         self._indices = np.random.permutation(self._data_size)
 
-    def print_batch(self, batch):
+    def print_batch(self, batch, stop_after_EOS=False):
         s = []
         for sentence in batch:
-            s.append(" ".join([self.dataset.idx_2_word(w) for w in sentence.tolist()]))
+            sentence = sentence.tolist()
+            if stop_after_EOS:
+                sentence_ = []
+                for w in sentence:
+                    if w != self.dataset.word_2_idx(self.dataset.EOS):
+                        sentence_.append(w)
+                    else:
+                        sentence = sentence_
+                        break
+            sent = " ".join([self.dataset.idx_2_word(w) for w in sentence])
+            s.append(sent)
         return s
 
-def load_dataset(config, type_='train', sorted_words=None):
-    assert type_ in ['train', 'test', 'train_eval'], 'Type must be train/test/train_eval'
+
+def load_dataset(config, type_='train', sorted_words=None, dropout_rate=0.2):
+    assert type_ in ['train', 'test', 'train_eval', 'validation'], 'Type must be train/test/train_eval/validation'
 
     if type_ == 'train':
         dataset = Dataset('data')
-        data_loader = DataLoader(dataset, batch_size=config.batch_size, word_dropout=0.25)
+        data_loader = DataLoader(dataset, batch_size=config.batch_size, word_dropout=dropout_rate)
     elif type_ == 'test':
         dataset = Dataset('data', file_='23.auto.clean', sorted_words=sorted_words)
+        data_loader = DataLoader(dataset, batch_size=config.batch_size)
+    elif type_ == 'validation':
+        dataset = Dataset('data', file_='22.auto.clean', sorted_words=sorted_words)
         data_loader = DataLoader(dataset, batch_size=config.batch_size)
     elif type_ == 'train_eval':
         dataset = Dataset('data', sorted_words=sorted_words)

Assignment2/metrics.py
@@ -13,31 +13,54 @@ def ACC(predictions, targets, masks, lengths):
 def eval_RNN(out, targets, mask):
     """ Compute perplexity """
     #Collect the probs of the targets.
-    out = torch.nn.functional.softmax(out, dim=-1)
-    likelihood = out.gather(dim=-1, index=targets.unsqueeze(-1)).squeeze()
-    likelihood = likelihood.log().sum(0)
-    return -likelihood.mean(), -likelihood.sum()/mask.sum()
 
-def eval_VAE(SentVAE, inputs, targets, mask, S=10):
+    logits_flat = out.view(-1, out.size(-1))
+    # log_probs_flat: (batch * max_len, num_classes)
+    log_probs_flat = torch.nn.functional.log_softmax(logits_flat, dim=1)
+    # target_flat: (batch * max_len, 1)
+    target_flat = targets.contiguous().view(-1, 1)
+    # losses_flat: (batch * max_len, 1)
+    losses_flat = -torch.gather(log_probs_flat, dim=1, index=target_flat)
+    # losses: (batch, max_len)
+    losses = losses_flat.view(*targets.size())
+    # mask: (batch, max_len)
+    prob = losses * mask.float()
+
+    likelihood = prob.sum(0)
+    ppl = prob.sum()
+
+    return -likelihood.mean(), ppl
+
+def eval_VAE(SentVAE, inputs, targets, mask, device, S=10):
     mu, log_sigma = SentVAE.encoder(inputs, mask.sum(0))
 
-    prior = torch.distributions.normal.Normal(torch.zeros(inputs.size(1), SentVAE.latent_size), 1)
+    prior = torch.distributions.normal.Normal(torch.zeros(inputs.size(1), SentVAE.latent_size).to(device), 1)
     posterior = torch.distributions.normal.Normal(mu, log_sigma.exp())
 
-    likelihood = torch.zeros(S, mask.size(1))
+    likelihood = torch.zeros(S, mask.size(1)).to(device)
     for k in range(S):
-        z = posterior.sample()
+        z = posterior.sample().to(device)
         out = SentVAE.decoder(inputs, z, mask.sum(0))
-        out = torch.nn.functional.softmax(out, dim=-1)
-        p = out.gather(dim=-1, index=targets.unsqueeze(-1)).squeeze().log().sum(0)
+        
+        logits_flat = out.view(-1, out.size(-1))
+        # log_probs_flat: (batch * max_len, num_classes)
+        log_probs_flat = torch.nn.functional.log_softmax(logits_flat, dim=1)
+        # target_flat: (batch * max_len, 1)
+        target_flat = targets.contiguous().view(-1, 1)
+        # losses_flat: (batch * max_len, 1)
+        losses_flat = -torch.gather(log_probs_flat, dim=1, index=target_flat)
+        # losses: (batch, max_len)
+        losses = losses_flat.view(*targets.size())
+        # mask: (batch, max_len)
+        p = (losses * mask.float()).sum(0)
+
         q = posterior.log_prob(z).sum(-1)
         prior_p = prior.log_prob(z).sum(-1)
         likelihood[k] = (p + prior_p - q)
 
     likelihood = (torch.logsumexp(likelihood, dim=0) - np.log(S))
 
-
-    return -likelihood.mean(), -likelihood.sum()/mask.sum()
+    return -likelihood.mean(), likelihood.sum()
 
 
 def compute_loss(logits, target, mask):

Assignment2/sent_vae.py
@@ -14,10 +14,14 @@ class SentVAE(nn.Module):
         self.encoder = Encoder(vocab_size, emb_size, hidden_size, latent_size, num_layers, pad_idx)
         self.decoder = Decoder(vocab_size, emb_size, hidden_size, latent_size, num_layers, pad_idx, sos_idx)
 
-    def forward(self, input, lengths):
+    def forward(self, input, lengths, greedy=False):
         mu, sigma = self.encoder(input, lengths)
         eps = torch.normal(torch.zeros_like(mu)).to(self.device)
-        z = mu + 0.5*sigma.exp() * eps
+
+        if greedy:
+            z = mu
+        else:
+            z = mu + 0.5*sigma.exp() * eps
 
         out = self.decoder(input, z, lengths)
         return out, mu, sigma

Assignment2/train_vae.py
@@ -16,6 +16,7 @@ def evaluate(model, data_loader, dataset, device):
     accuracy = 0
     perplexity = 0
     likelihood = 0
+    sum_lengths = 0
 
     num_samples = len(dataset)
 
@@ -31,26 +32,31 @@ def evaluate(model, data_loader, dataset, device):
             batch_targets = batch_targets.t().to(device)
             masks = masks.t().to(device)
             lengths = lengths.to(device)
-            predictions, mu, sigma = model.forward(batch_inputs, lengths)
+            predictions, mu, sigma = model.forward(batch_inputs, lengths, greedy=True)
             predicted_targets = predictions.argmax(dim=-1)
 
             N = batch_inputs.size(1)
 
             accuracy += metrics.ACC(predicted_targets, batch_targets, masks, lengths)*N
-            ll, ppl = metrics.eval_VAE(model, batch_inputs, batch_targets, masks)
-            perplexity += ppl*N
+            ll, ppl = metrics.eval_VAE(model, batch_inputs, batch_targets, masks, device)
+            perplexity += ppl
+            sum_lengths += lengths.sum()
             likelihood += ll*N
 
 
-    return accuracy.item()/num_samples, np.exp(perplexity.item()/num_samples), likelihood.item()/num_samples
+    return accuracy.item()/num_samples, np.exp(perplexity.item()/sum_lengths.item()), likelihood.item()/num_samples
 
 
 def train(config):
     print("Thank you for choosing the Sentence VAE today!")
+    print(config)
+    print()
+
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    dataset, data_loader = load_dataset(config, type_='train')
+    dataset, data_loader = load_dataset(config, type_='train', dropout_rate=config.word_dropout)
     dataset_test_eval, data_loader_test_eval = load_dataset(config, type_='test', sorted_words=dataset.sorted_words)
+    dataset_validation_eval, data_loader_validation_eval = load_dataset(config, type_='validation', sorted_words=dataset.sorted_words)
     dataset_train_eval, data_loader_train_eval = load_dataset(config, type_='train_eval', sorted_words=dataset.sorted_words)
 
     print("Size of train dataset: %d" % len(dataset))
@@ -89,7 +95,7 @@ def train(config):
 
         loss.backward()
         optimizer.step()
-        scheduler.step()
+        #scheduler.step()
 
         loss_sum += loss.item()
         loss_kl_sum += kl_loss.item()
@@ -117,7 +123,7 @@ def train(config):
 
             print("%s\nSAMPLES:" % ("-"*60))
             sample = model.sample()
-            sample = data_loader.print_batch(sample.t())
+            sample = data_loader.print_batch(sample.t(), stop_after_EOS=True)
             for s in sample:
                 print(s)
                 print()
@@ -125,17 +131,21 @@ def train(config):
 
             print("%s\nINTERPOLATION:" % ("-"*60))
             result = model.interpolation(n_steps=10)
-            result = data_loader.print_batch(result.t())
+            result = data_loader.print_batch(result.t(), stop_after_EOS=True)
             for s in result:
                 print(s)
                 print()
 
 
-        if step % 10000 == 0:
+        if step % 5000 == 0:
             eval_acc, eval_ppl, eval_ll = evaluate(model, data_loader_test_eval, dataset_test_eval, device)
+            val_acc, val_ppl, val_ll = evaluate(model, data_loader_validation_eval, dataset_validation_eval, device)
             train_acc, train_ppl, train_ll = evaluate(model, data_loader_train_eval, dataset_train_eval, device)
 
-            print("Train accuracy-perplexity_likelihood: %.3f %.3f %.3f Test accuracy-perplexity-likelihood: %.3f %.3f %.3f" % (train_acc, train_ppl, train_ll, eval_acc, eval_ppl, eval_ll))
+            print("Train accuracy-perplexity_likelihood: %.3f %.3f %.3f" % (eval_acc, eval_ppl, eval_ll))
+            print("Test accuracy-perplexity-likelihood: %.3f %.3f %.3f" % (train_acc, train_ppl, train_ll))
+            print("Validation accuracy-perplexity-likelihood: %.3f %.3f %.3f" % (val_acc, val_ppl, val_ll))
+
             torch.save(model.state_dict(), 'vae-model-%d.pt' % step)
 
         if data_loader.epoch == config.epochs:
@@ -148,17 +158,19 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     # Model params
-    parser.add_argument('--num_hidden', type=int, default=400, help='Number of hidden units in the LSTM')
+    parser.add_argument('--num_hidden', type=int, default=100, help='Number of hidden units in the LSTM')
     parser.add_argument('--num_layers', type=int, default=2, help='Number of LSTM layers in the model')
 
     # Training params
     parser.add_argument('--batch_size', type=int, default=20, help='Number of examples to process in a batch')
     parser.add_argument('--learning_rate', type=float, default=0.001, help='Learning rate')
+    parser.add_argument('--word_dropout', type=float, default=0.2, help='Learning rate')
+
 
     # It is not necessary to implement the following three params, but it may help training.
     parser.add_argument('--learning_rate_decay', type=float, default=0.96, help='Learning rate decay fraction')
     parser.add_argument('--learning_rate_step', type=int, default=1000, help='Learning rate step')
-    parser.add_argument('--epochs', type=int, default=30, help='Number of training epochs')
+    parser.add_argument('--epochs', type=int, default=20, help='Number of training epochs')
 
     # Misc params
     parser.add_argument('--print_every', type=int, default=1000, help='How often to print training progress')